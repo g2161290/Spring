20e40a00d9dbb0ff5bf19a3957f075069df76ba9
g2161290 <141694702+g2161290@users.noreply.github.com>
2023-10-23 17:50:50 +0900

    1023

--- sp01/src/main/java/com/yedam/app/emp/service/EmpVO.java
+++ sp01/src/main/java/com/yedam/app/emp/service/EmpVO.java
@@ -1,15 +1,28 @@
 package com.yedam.app.emp.service;
 
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
 public class EmpVO {
-	private String employee_id;
-	private String first_name;
-	private String last_name;
+	/*
+	 * private String employee_id; private String first_name; private String
+	 * last_name; private String email; private String hire_date; private String
+	 * job_id; private String department_id; private String salary;
+	 */
+	// 1) 컬럼따라 타입을 구분 or 실제타입대신 String
+	// 2) 기본타입(int, double 등)or Wrap클래스(Integer, Double)-null허용
+	private int employeeId;
+	private String firstName;
+	private String lastName;
 	private String email;
-	private String hire_date;
-	private String job_id;
-	private String department_id;
-	private String salary;
+	private String phoneNumber;
+	private Date hireDate;
+	private String jobId;
+	private double salary;
+	private Double commissionPct;
+	private Integer managerId;
+	private Integer departmentID;
+	
 }
